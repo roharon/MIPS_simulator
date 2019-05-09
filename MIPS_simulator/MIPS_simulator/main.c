a7f972f89c051d42a75c1c05efc3261c8e7423be
roharon <roharon98@gmail.com>
2019-05-09 21:21:09 +0900

    Create main.c

---
+++ MIPS_simulator/MIPS_simulator/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+Program counter는 동일하게 구현
+
+MIPS명령어
+add $t0, $t0, $t2
+
+하면 결과 나오게
+
+레지스터 공간 구현해야한다.
+
+ALU
+*/
+
+/*
+$zero  0
+$v0~$v1 2~3
+$a0~$a3 4~7
+$t0 ~ $t7 8~15
+$s0~$s7 16~23
+$t8~$t9 24~25
+$gp 28
+$sp 29
+$fp 30
+$ra 31
+*/
+
+int main() {
+	
+}
